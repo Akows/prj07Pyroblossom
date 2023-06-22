21f319cca76165fb4e5b40fde54bda15dae9bdfd
AKOWSPC <akows141@gmail.com>
2023-06-22 19:57:36 +0900

    [스토어 마이페이지](UI)(기능) - 제품 구매 내역이 로그인한 사용자의 것만 출력되지 않는 문제가 발생, 해결. (쿼리 기준에 오타가 있었음. DB마다 PK으로 사용하는 부분은 명칭을 동일하게 했어야 했는데, 필드명 지정할때 실수가 있었음.)

--- src/redux/actions/storeAction.js
+++ src/redux/actions/storeAction.js
@@ -583,12 +583,12 @@ const GetpurchaseRecord = (listCallType, itemPerPage, searchKeyword, userEmail) 
             let LastQueryRef = '';
 
             if (listCallType === 'keywordsearch') {
-                firstQueryRef = query(purchaseRecordCollectionRef, orderBy('date', 'asc'), where('name', '==', searchKeyword), where('userEmail', '==', userEmail), limit(1));
-                LastQueryRef = query(purchaseRecordCollectionRef, orderBy('date', 'asc'), where('name', '==', searchKeyword), where('userEmail', '==', userEmail), limitToLast(1));
+                firstQueryRef = query(purchaseRecordCollectionRef, orderBy('date', 'asc'), where('name', '==', searchKeyword), where('userName', '==', userEmail), limit(1));
+                LastQueryRef = query(purchaseRecordCollectionRef, orderBy('date', 'asc'), where('name', '==', searchKeyword), where('userName', '==', userEmail), limitToLast(1));
             }
             else {
-                firstQueryRef = query(purchaseRecordCollectionRef, orderBy('date', 'asc'), where('userEmail', '==', userEmail), limit(1));
-                LastQueryRef = query(purchaseRecordCollectionRef, orderBy('date', 'asc'), where('userEmail', '==', userEmail), limitToLast(1));
+                firstQueryRef = query(purchaseRecordCollectionRef, orderBy('date', 'asc'), where('userName', '==', userEmail), limit(1));
+                LastQueryRef = query(purchaseRecordCollectionRef, orderBy('date', 'asc'), where('userName', '==', userEmail), limitToLast(1));
             };
             
             const firstDocumentSnapshots = await getDocs(firstQueryRef);
@@ -607,16 +607,16 @@ const GetpurchaseRecord = (listCallType, itemPerPage, searchKeyword, userEmail) 
             // 최초 랜더링일 때, itemPerPage만큼 데이터를 조회해온다.
             // 만약 검색어를 입력하지 않았는데 검색 버튼을 클릭할 경우에도 마찬가지로 처리한다.
             if (listCallType === 'firstRender' || searchKeyword === '') {
-                queryRef = query(purchaseRecordCollectionRef, orderBy('date'), where('userEmail', '==', userEmail), limit(itemPerPage));
+                queryRef = query(purchaseRecordCollectionRef, orderBy('date'), where('userName', '==', userEmail), limit(itemPerPage));
             }
             // 검색일 때, where 함수를 사용하여 조건검색으로 데이터를 조회해온다.
             else if (listCallType === 'keywordsearch') {
-                queryRef = query(purchaseRecordCollectionRef, orderBy('date'), where('name', '==', searchKeyword), where('userEmail', '==', userEmail), limit(itemPerPage));
+                queryRef = query(purchaseRecordCollectionRef, orderBy('date'), where('name', '==', searchKeyword), where('userName', '==', userEmail), limit(itemPerPage));
             };
 
             // 일반 유저화면에서의 제품 로딩의 경우, 비공개된 제품이 출력되서는 안된다.
             if (listCallType === 'commonusergetproduct') {
-                queryRef = query(purchaseRecordCollectionRef, orderBy('date'), where('productDisclosure', '==', true), where('userEmail', '==', userEmail), limit(itemPerPage));
+                queryRef = query(purchaseRecordCollectionRef, orderBy('date'), where('productDisclosure', '==', true), where('userName', '==', userEmail), limit(itemPerPage));
             };
 
             // 다른 페이지로 이동할 경우, 페이지 이동을 위한 데이터 Index를 바탕으로 데이터를 조회해온다.
@@ -624,10 +624,10 @@ const GetpurchaseRecord = (listCallType, itemPerPage, searchKeyword, userEmail) 
             // 따라서 당 정보를 가져와서 사용한다.
             const { firstOfPage, lastOfPage } = getState().store.processInfo.processData1;
             if (listCallType === 'next') {
-                queryRef = query(purchaseRecordCollectionRef, orderBy('date'), where('userEmail', '==', userEmail), startAfter(lastOfPage), limit(itemPerPage));
+                queryRef = query(purchaseRecordCollectionRef, orderBy('date'), where('userName', '==', userEmail), startAfter(lastOfPage), limit(itemPerPage));
             }
             else if (listCallType === 'prev') {
-                queryRef = query(purchaseRecordCollectionRef, orderBy('date'), where('userEmail', '==', userEmail), endBefore(firstOfPage), limitToLast(itemPerPage));
+                queryRef = query(purchaseRecordCollectionRef, orderBy('date'), where('userName', '==', userEmail), endBefore(firstOfPage), limitToLast(itemPerPage));
             };
             
             // 그리고 쿼리를 기준으로 Doc을 가져온다.
@@ -645,9 +645,6 @@ const GetpurchaseRecord = (listCallType, itemPerPage, searchKeyword, userEmail) 
                 data.date = dateFormat(doc.data().date.toDate());
                 result.push(data);
             });
-
-            console.log(result);
-
             returnData.processData2 = result;
         };
 
